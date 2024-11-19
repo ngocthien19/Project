723dd742bb859125b6e8d77212bfbf97166cdfc9
Cao Ngoc Thien <caongocthien1902@gmail.com>
2024-11-19 17:46:20 +0700

    UI feature

--- features.py
+++ features.py
@@ -161,7 +161,6 @@ def FeaturesPage(root):
     featuresPage = tk.Toplevel()  
     featuresPage.title("Features")
     featuresPage.geometry("1300x700")
-    featuresPage.resizable(False, False)
     featuresPage.config(bg="#FFF")
 
     # Fonts
